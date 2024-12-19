bae052f47a9ff5b813f993821ad8c9a8c811d688
De6ris <2838369930@qq.com>
2024-12-19 13:40:12 +0800

    merge from 1.21 and adapt to 1.21.4

--- src/main/java/com/github/Debris/DebrisClient/mixin/item/TridentItemMixin.java
+++ src/main/java/com/github/Debris/DebrisClient/mixin/item/TridentItemMixin.java
@@ -10,12 +10,12 @@ import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(TridentItem.class)
 public class TridentItemMixin {
     @Inject(method = "onStoppedUsing", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;incrementStat(Lnet/minecraft/stat/Stat;)V"))
-    private void onTridentLaunch(ItemStack stack, World world, LivingEntity user, int remainingUseTicks, CallbackInfo ci) {
+    private void onTridentLaunch(ItemStack stack, World world, LivingEntity user, int remainingUseTicks, CallbackInfoReturnable<Boolean> cir) {
         if (world.isClient()) {
             float f = EnchantmentHelper.getTridentSpinAttackStrength(stack, user);
             if (f == 0.0f) {// thrown out, rather than spin sprint
