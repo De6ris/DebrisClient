afa256fa72684003f67073702fdb104449dd1253
De6ris <2838369930@qq.com>
2024-12-21 17:44:37 +0800

    fix : tooltip for ConfigOptionList crash out of dev env

--- src/main/java/com/github/Debris/DebrisClient/util/AccessorUtil.java
+++ src/main/java/com/github/Debris/DebrisClient/util/AccessorUtil.java
@@ -1,11 +1,7 @@
 package com.github.Debris.DebrisClient.util;
 
-import com.github.Debris.DebrisClient.config.api.IIConfigOptionList;
 import com.github.Debris.DebrisClient.mixin.gui.IMixinChatHud;
 import com.github.Debris.DebrisClient.mixin.gui.IMixinGuiContainer;
-import com.google.common.collect.ImmutableList;
-import fi.dy.masa.malilib.config.IConfigOptionList;
-import fi.dy.masa.malilib.config.IConfigOptionListEntry;
 import net.minecraft.client.gui.hud.ChatHud;
 import net.minecraft.client.gui.hud.ChatHudLine;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
@@ -24,9 +20,5 @@ public class AccessorUtil {
 
     public static List<ChatHudLine.Visible> getVisibleMessages(ChatHud chatHud) {
         return ((IMixinChatHud) chatHud).getVisibleMessages();
-    }
-
-    public static ImmutableList<IConfigOptionListEntry> getConfigOptionListEntries(IConfigOptionList config) {
-        return ((IIConfigOptionList) config).dc$getEntries();
     }
 }
