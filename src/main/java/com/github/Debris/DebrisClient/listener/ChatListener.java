f241c7d182c3e98ab7f96220c21e8cddaca4751f
De6ris <2838369930@qq.com>
2024-12-23 00:08:12 +0800

    feat: AutoRepeatAntiDDos, to keep safe

--- src/main/java/com/github/Debris/DebrisClient/listener/ChatListener.java
+++ src/main/java/com/github/Debris/DebrisClient/listener/ChatListener.java
@@ -6,11 +6,20 @@ import com.github.Debris.DebrisClient.util.ChatUtil;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.Text;
 
-import java.util.Comparator;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class ChatListener {
+    private static final Map<String, List<Long>> TIME_STAMP_MAP = new ConcurrentHashMap<>();
+
     public static void onMessageAdd(MinecraftClient client, Text message) {
         handleAutoRepeat(client, message);
+    }
+
+    public static void onClientTick(MinecraftClient client) {
+        long currentTime = System.currentTimeMillis();
+        TIME_STAMP_MAP.values().forEach(x -> x.removeIf(y -> currentTime - y > 1000));
+        TIME_STAMP_MAP.values().removeIf(List::isEmpty);
     }
 
     private static void handleAutoRepeat(MinecraftClient client, Text message) {
@@ -20,6 +29,30 @@ public class ChatListener {
                 .map(ChatUtil::angleName)
                 .flatMap(x -> ChatUtil.filterMessageContent(x, originalString).stream())
                 .max(Comparator.comparing(String::length))
-                .ifPresent(x -> ChatUtil.sendChat(client, x));
+                .ifPresent(x -> sendChatCheckDDos(client, x));
+    }
+
+    private static void sendChatCheckDDos(MinecraftClient client, String message) {
+        boolean canSend = false;
+        if (DCCommonConfig.AutoRepeatAntiDDos.getBooleanValue()) {
+            long currentTime = System.currentTimeMillis();
+            if (TIME_STAMP_MAP.containsKey(message)) {
+                List<Long> list = TIME_STAMP_MAP.get(message);
+                if (list.size() < DCCommonConfig.AutoRepeatAntiDDosThreshold.getIntegerValue()) {
+                    canSend = true;
+                    list.add(currentTime);
+                }
+            } else {
+                List<Long> list = new ArrayList<>();
+                list.add(currentTime);
+                TIME_STAMP_MAP.put(message, Collections.synchronizedList(list));
+                canSend = true;
+            }
+        } else {
+            canSend = true;
+        }
+        if (canSend) {
+            ChatUtil.sendChat(client, message);
+        }
     }
 }
