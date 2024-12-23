708fd83afeb41804ac9e22c276a50ef713e437e3
De6ris <2838369930@qq.com>
2024-12-23 21:55:38 +0800

    feat: auto throw

--- src/main/java/com/github/Debris/DebrisClient/listener/TickListener.java
+++ src/main/java/com/github/Debris/DebrisClient/listener/TickListener.java
@@ -4,21 +4,17 @@ import com.github.Debris.DebrisClient.config.DCCommonConfig;
 import com.github.Debris.DebrisClient.inventory.stoneCutter.StoneCutterUtil;
 import com.github.Debris.DebrisClient.unsafe.itemScroller.MassCraftingApi;
 import com.github.Debris.DebrisClient.util.BotUtil;
+import com.github.Debris.DebrisClient.util.MiscUtil;
 import com.github.Debris.DebrisClient.util.StringUtil;
 import com.github.Debris.DebrisClient.util.TridentUtil;
 import fi.dy.masa.malilib.interfaces.IClientTickHandler;
 import net.fabricmc.loader.api.FabricLoader;
-import net.fabricmc.loader.api.ModContainer;
-import net.fabricmc.loader.api.Version;
-import net.fabricmc.loader.api.VersionParsingException;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 
-import java.util.Optional;
-
 public class TickListener implements IClientTickHandler {
     @Override
-    public void onClientTick(MinecraftClient minecraftClient) {
+    public void onClientTick(MinecraftClient client) {
         if (DCCommonConfig.CutStoneThenDrop.getKeybind().isKeybindHeld() || DCCommonConfig.StartStoneCutting.getBooleanValue()) {
             if (StoneCutterUtil.isStoneCutterGui()) {
                 StoneCutterUtil.cutStoneThenDrop();
@@ -35,13 +31,17 @@ public class TickListener implements IClientTickHandler {
 
         if (DCCommonConfig.KickBot.getKeybind().isKeybindHeld()) {
             if (FabricLoader.getInstance().isModLoaded("tweakeroo")) {
-                BotUtil.tryKickBot(minecraftClient);
+                BotUtil.tryKickBot(client);
             }
         }
 
-        TridentUtil.onClientTick(minecraftClient);
+        if (DCCommonConfig.AutoThrow.getBooleanValue() && client.world != null && client.currentScreen == null) {
+            MiscUtil.runAutoThrow();
+        }
 
-        ChatListener.onClientTick(minecraftClient);
+        TridentUtil.onClientTick(client);
+
+        ChatListener.onClientTick(client);
     }
 
 
