e065544320a7b0109cf6b6cab33a10e7c0625da8
De6ris <2838369930@qq.com>
2025-03-13 14:49:41 +0800

    portal gen is monitored better

--- src/main/java/com/github/Debris/DebrisClient/mixin/world/PortalForcerMixin.java
+++ src/main/java/com/github/Debris/DebrisClient/mixin/world/PortalForcerMixin.java
@@ -2,14 +2,16 @@ package com.github.Debris.DebrisClient.mixin.world;
 
 import com.github.Debris.DebrisClient.config.DCCommonConfig;
 import com.github.Debris.DebrisClient.util.ChatUtil;
-import net.minecraft.screen.ScreenTexts;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.BlockLocating;
 import net.minecraft.world.dimension.PortalForcer;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -18,12 +20,19 @@ import java.util.Optional;
 
 @Mixin(PortalForcer.class)
 public class PortalForcerMixin {
+    @Shadow
+    @Final
+    private ServerWorld world;
+
     @SuppressWarnings("all")
     @Inject(method = "createPortal", at = @At(value = "RETURN", ordinal = 1))
     private void onPortalCreated(BlockPos pos, Direction.Axis axis, CallbackInfoReturnable<Optional<BlockLocating.Rectangle>> cir) {
         if (DCCommonConfig.MonitorPortalGeneration.getBooleanValue()) {
-            String string = cir.getReturnValue().get().lowerLeft.toString();
-            ChatUtil.addLocalMessage(Text.empty().append(Text.translatable("debug.prefix").formatted(Formatting.YELLOW, Formatting.BOLD)).append(ScreenTexts.SPACE).append(string));
+            BlockPos lowerLeft = cir.getReturnValue().get().lowerLeft;
+            ChatUtil.addLocalMessage(Text.empty()
+                    .append(Text.translatable("debug.prefix").formatted(Formatting.YELLOW, Formatting.BOLD))
+                    .append(String.format(" portal (%s) created in %s", lowerLeft.toShortString(), this.world.getRegistryKey().getValue()))
+            );
         }
     }
 }
