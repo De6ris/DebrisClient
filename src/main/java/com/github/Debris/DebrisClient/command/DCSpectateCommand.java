143eab82f7f64c855fd12ee134883957bba2c9d5
De6ris <2838369930@qq.com>
2025-03-17 18:36:46 +0800

    better spectate command

--- src/main/java/com/github/Debris/DebrisClient/command/DCSpectateCommand.java
+++ src/main/java/com/github/Debris/DebrisClient/command/DCSpectateCommand.java
@@ -1,24 +1,25 @@
 package com.github.Debris.DebrisClient.command;
 
 import com.github.Debris.DebrisClient.util.InteractionUtil;
+import com.mojang.authlib.GameProfile;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
 import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
-import dev.xpple.clientarguments.arguments.CEntitySelector;
+import dev.xpple.clientarguments.arguments.CEntityArgument;
+import dev.xpple.clientarguments.arguments.CGameProfileArgument;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.Text;
 
-import static dev.xpple.clientarguments.arguments.CEntityArgument.player;
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.argument;
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
 
 /**
- * Same to vanilla spectate command, but no permission required. The target must be within client world though.
+ * Same to vanilla spectate command, but no permission required.
  */
 public class DCSpectateCommand {
     private static final SimpleCommandExceptionType SPECTATE_SELF_EXCEPTION = new SimpleCommandExceptionType(Text.translatable("commands.spectate.self"));
@@ -28,26 +29,32 @@ public class DCSpectateCommand {
 
     public static void register(CommandDispatcher<FabricClientCommandSource> dispatcher) {
         dispatcher.register(literal(Commands.PREFIX + "spectate")
-                .then(argument("filter", player())
-                        .executes(ctx -> processCommand(ctx.getSource(), ctx.getArgument("filter", CEntitySelector.class))))
+                .then(argument("filter", CGameProfileArgument.gameProfile(true))
+                        .executes(ctx -> processCommand(ctx.getSource(), ctx.getArgument("filter", CGameProfileArgument.Result.class))))
         );
     }
 
-    private static int processCommand(FabricClientCommandSource source, CEntitySelector filter) throws CommandSyntaxException {
-        ClientPlayerEntity player = source.getPlayer();
-        AbstractClientPlayerEntity target = filter.findSinglePlayer(source);
-        if (player == target) {
+    private static int processCommand(FabricClientCommandSource source, CGameProfileArgument.Result filter) throws CommandSyntaxException {
+        ClientPlayerEntity self = source.getPlayer();
+        GameProfile targetGameProfile = filter.getNames(source).stream().findFirst().orElseThrow(CEntityArgument.PLAYER_NOT_FOUND_EXCEPTION::create);
+        if (self.getGameProfile().getName().equals(targetGameProfile.getName())) {
             throw SPECTATE_SELF_EXCEPTION.create();
         }
-        if (!player.isSpectator()) {
-            throw NOT_SPECTATOR_EXCEPTION.create(player.getDisplayName());
+
+        if (!self.isSpectator()) {
+            throw NOT_SPECTATOR_EXCEPTION.create(self.getDisplayName());
         }
+
         MinecraftClient client = source.getClient();
-        if (!player.canInteractWithEntityIn(target.getBoundingBox(), 3.0D)) {
-            InteractionUtil.spectatorTeleport(client, target.getUuid());
+        InteractionUtil.spectatorTeleport(client, targetGameProfile.getId());
+
+        PlayerEntity targetPlayerEntity = source.getWorld().getPlayerByUuid(targetGameProfile.getId());
+        if (targetPlayerEntity == null) {
+            source.sendFeedback(Text.literal("未在附近找到该玩家实体, 已请求向该玩家传送, 请在传送完成后重试指令"));
+        } else {
+            InteractionUtil.attackEntity(client, targetPlayerEntity);// the server will set your camera
         }
-        InteractionUtil.attackEntity(client, target);
+
         return Command.SINGLE_SUCCESS;
     }
-
 }
