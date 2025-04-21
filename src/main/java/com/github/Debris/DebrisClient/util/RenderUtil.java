fcebb378cbe997df94828be6840dc91ccc41ae8d
De6ris <2838369930@qq.com>
2025-04-21 13:42:00 +0800

    fix connect line too slim; update dependency

--- src/main/java/com/github/Debris/DebrisClient/util/RenderUtil.java
+++ src/main/java/com/github/Debris/DebrisClient/util/RenderUtil.java
@@ -6,7 +6,6 @@ import fi.dy.masa.malilib.render.RenderUtils;
 import fi.dy.masa.malilib.util.data.Color4f;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.BufferBuilder;
-import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.Vec3d;
 import org.jetbrains.annotations.NotNull;
 
@@ -24,10 +23,9 @@ public class RenderUtil {
         RenderUtils.color(1f, 1f, 1f, 1f);
         RenderUtils.blend(true);
 
-        RenderContext ctx = new RenderContext(MaLiLibPipelines.LINES_MASA_SIMPLE_NO_DEPTH_NO_CULL);
+        RenderContext ctx = new RenderContext(MaLiLibPipelines.DEBUG_LINES_MASA_SIMPLE_NO_DEPTH_NO_CULL);
         BufferBuilder builder = ctx.getBuilder();
-        MatrixStack matrixStack = new MatrixStack();
-        MatrixStack.Entry e = matrixStack.peek();
+
         RenderUtils.drawBoxAllEdgesBatchedLines(
                 (float) (pos1.getX() - boxLength),
                 (float) (pos1.getY() - boxLength),
@@ -36,8 +34,7 @@ public class RenderUtil {
                 (float) (pos1.getY() + boxLength),
                 (float) (pos1.getZ() + boxLength),
                 pos1Color,
-                builder,
-                e
+                builder
         );
 
         RenderUtils.drawBoxAllEdgesBatchedLines(
@@ -48,12 +45,11 @@ public class RenderUtil {
                 (float) (pos2.getY() + boxLength),
                 (float) (pos2.getZ() + boxLength),
                 pos2Color,
-                builder,
-                e
+                builder
         );
 
-        builder.vertex((float) pos1.getX(), (float) pos1.getY(), (float) pos1.getZ()).color(lineColor.r, lineColor.g, lineColor.b, lineColor.a).normal(e, 0.0f, 0.0f, 0.0f);
-        builder.vertex((float) pos2.getX(), (float) pos2.getY(), (float) pos2.getZ()).color(lineColor.r, lineColor.g, lineColor.b, lineColor.a).normal(e, 0.0f, 0.0f, 0.0f);
+        builder.vertex((float) pos1.getX(), (float) pos1.getY(), (float) pos1.getZ()).color(lineColor.r, lineColor.g, lineColor.b, lineColor.a);
+        builder.vertex((float) pos2.getX(), (float) pos2.getY(), (float) pos2.getZ()).color(lineColor.r, lineColor.g, lineColor.b, lineColor.a);
 
         try {
             ctx.draw(builder.endNullable());
@@ -62,6 +58,6 @@ public class RenderUtil {
         }
 
         RenderUtils.depthTest(true);
-    }// TODO lines too slim
+    }
 
 }
