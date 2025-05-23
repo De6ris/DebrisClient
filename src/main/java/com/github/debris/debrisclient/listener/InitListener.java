e595602bf333ff556ae0a943f6eeb1fa1ecfdcd4
De6ris <2838369930@qq.com>
2025-05-23 16:56:58 +0800

    adapt to malilib config switcher

--- src/main/java/com/github/debris/debrisclient/listener/InitListener.java
+++ src/main/java/com/github/debris/debrisclient/listener/InitListener.java
@@ -3,12 +3,15 @@ package com.github.debris.debrisclient.listener;
 import com.github.debris.debrisclient.DebrisClient;
 import com.github.debris.debrisclient.config.Callbacks;
 import com.github.debris.debrisclient.config.DCCommonConfig;
+import com.github.debris.debrisclient.gui.DCConfigUi;
 import fi.dy.masa.malilib.config.ConfigManager;
 import fi.dy.masa.malilib.event.InputEventHandler;
 import fi.dy.masa.malilib.event.RenderEventHandler;
 import fi.dy.masa.malilib.event.TickHandler;
 import fi.dy.masa.malilib.event.WorldLoadHandler;
 import fi.dy.masa.malilib.interfaces.IInitializationHandler;
+import fi.dy.masa.malilib.registry.Registry;
+import fi.dy.masa.malilib.util.data.ModInfo;
 import net.minecraft.client.MinecraftClient;
 
 public class InitListener implements IInitializationHandler {
@@ -23,5 +26,6 @@ public class InitListener implements IInitializationHandler {
         WorldLoadHandler.getInstance().registerWorldLoadPreHandler(WorldLoadListener.getInstance());
         WorldLoadHandler.getInstance().registerWorldLoadPostHandler(WorldLoadListener.getInstance());
         RenderEventHandler.getInstance().registerWorldLastRenderer(RenderListener.getInstance());
+        Registry.CONFIG_SCREEN.registerConfigScreenFactory(new ModInfo(DebrisClient.MOD_ID, DebrisClient.MOD_NAME, DCConfigUi::new));
     }
 }
