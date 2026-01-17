4069bedb94083a735b9d924dc6b7084d4c79f84d
De6ris <2838369930@qq.com>
2026-01-17 14:45:07 +0800

    rename mod menu impl

--- src/main/java/com/github/debris/debrisclient/compat/ModMenuImpl.java
+++ src/main/java/com/github/debris/debrisclient/compat/ModMenuImpl.java
@@ -4,7 +4,7 @@ import com.github.debris.debrisclient.gui.DCConfigUi;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 
-public class ModMenu implements ModMenuApi {
+public class ModMenuImpl implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
         return (screen) -> {
