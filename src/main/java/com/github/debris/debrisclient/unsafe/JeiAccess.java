d5954b7177eab643b25f2efef77e98923a9f8529
De6ris <2838369930@qq.com>
2026-09-25 00:03:44 +0800

    jei hovers

--- src/main/java/com/github/debris/debrisclient/unsafe/JeiAccess.java
+++ src/main/java/com/github/debris/debrisclient/unsafe/JeiAccess.java
@@ -2,16 +2,25 @@ package com.github.debris.debrisclient.unsafe;
 
 import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.runtime.IJeiRuntime;
+import mezz.jei.api.runtime.IRecipesGui;
 import net.minecraft.world.item.ItemStack;
+
+import java.util.Optional;
 
 public class JeiAccess {
     public static IJeiRuntime jeiRuntime;
 
+    @SuppressWarnings("OptionalIsPresent")
     public static ItemStack getHoveredStack() {
         ItemStack stack = jeiRuntime.getBookmarkOverlay().getItemStackUnderMouse();
         if (stack != null && !stack.isEmpty()) return stack;
+
         stack = jeiRuntime.getIngredientListOverlay().getIngredientUnderMouse(VanillaTypes.ITEM_STACK);
         if (stack != null && !stack.isEmpty()) return stack;
+
+        Optional<ItemStack> optional = jeiRuntime.getRecipesGui().getIngredientUnderMouse(VanillaTypes.ITEM_STACK);
+        if (optional.isPresent()) return optional.get();
+
         return ItemStack.EMPTY;
     }
 }
